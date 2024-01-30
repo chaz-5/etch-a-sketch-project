9209590178110959620be4985093259fa88c30cd
chaz-5 <143091579+chaz-5@users.noreply.github.com>
2024-01-30 21:05:32 +0000

    Add reset button that resets etch-a-sketch

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const gridCont = document.querySelector('.grid-cont');
 const inputBtn = document.querySelector('#input-btn');
+const inputBtnCont = document.querySelector('.input-btn-cont');
 let userInput = 0
+
+function refreshPage() {
+    window.location.reload();
+}
 
 function randomColor() {
     let letters = '0123456789ABCDEF';
@@ -32,16 +37,47 @@ for (let x = 0; x < 16; x++ ) {
 };
 
 // USER GRID
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'Reset';
+resetBtn.style.backgroundColor = 'lightcoral';
+resetBtn.style.color = 'white';
+resetBtn.style.fontFamily = 'Arial';
+resetBtn.style.fontSize = '20px';
+resetBtn.style.border = '0';
+resetBtn.style.paddingTop = '5px';
+resetBtn.style.paddingBottom = '5px';
+resetBtn.style.paddingLeft = '10px';
+resetBtn.style.paddingRight = '10px';
+resetBtn.style.marginTop = '20px';
+resetBtn.style.marginBottom = '50px';
+resetBtn.style.borderRadius = '5px';
+inputBtnCont.appendChild(resetBtn);
+resetBtn.addEventListener('mouseover', () => {
+    resetBtn.style.boxShadow = '0 0 8px black';
+});
+resetBtn.style.display = 'none';
+
 inputBtn.addEventListener('click', () => {
     document.body.removeChild(gridCont);
 
     userInput = prompt('Please type in your custom layout (max 100)');
+    while (userInput > 100) {
+        alert('Please type a number under 100');
+        userInput = prompt('Please type in your custom layout (max 100)');
+    }
 
     const userGridCont = document.createElement('div');
     userGridCont.setAttribute('class', 'usergrid-cont');
     userGridCont.style.display = 'flex';
     userGridCont.style.justifyContent = 'center';
     document.body.appendChild(userGridCont);
+
+    inputBtn.style.display = 'none';
+    resetBtn.style.display = 'inline';
+
+    resetBtn.addEventListener('click', () => {
+
+    })
 
     for (let x = 0; x < userInput; x++) {
         const userGridCont2 = document.createElement('div');
@@ -60,7 +96,11 @@ inputBtn.addEventListener('click', () => {
                 userGrid.style.backgroundColor = randomColor();
             });
         };
+        
     };
+    resetBtn.addEventListener('click', () => {
+        refreshPage();
+    });
 });
 
     
